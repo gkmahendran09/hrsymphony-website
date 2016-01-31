4928921e194a6dccd7d026a44b282b5ac9d6ab13
Mahendran Kannan <gkm@webkb.in>
2016-01-31 19:36:49 +0530

    1. First

---
+++ contact.php
@@ -0,0 +1,347 @@
+
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>HR`Symphony</title>
+    <link href="menu/component.css" rel="stylesheet" type="text/css" />
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="roll/script.js"></script>
+    <link rel="stylesheet" href="roll/animate.css" />
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="dist/css/bootstrapValidator.css"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/octicons/3.1.0/octicons.min.css">
+    <link rel="stylesheet" href="css/simple-sidebar.css">
+    <link href="menu/component.css" rel="stylesheet" type="text/css" />
+    <link href='https://fonts.googleapis.com/css?family=Montserrat:700' rel='stylesheet' type='text/css'>
+    <!--[if lt IE 9]>
+      <script src="https://cdn.jsdelivr.net/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://cdn.jsdelivr.net/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <link rel="stylesheet" href="clock/flipclock.css">
+    <style>
+    body ,#page-content-wrapper{
+      background: transparent url("images/themee.jpg") no-repeat;
+      background-size: cover;
+    }
+
+    header.smaller{
+      background-color: #ccc;
+    }
+    .navbar-default {
+      background: url("http://mssw.in/img/logo_header_bg.png") repeat;
+    }
+    .carousel-inner > .item > img,
+    .carousel-inner > .item > a > img {
+        width: 100%;
+        height:400px;
+        margin: auto;
+    }
+    .carousel {
+      top: 65;
+    }
+    .item1{
+      background:transparent  url("http://www.w3schools.com/bootstrap/img_flower.jpg") no-repeat;
+      background-size: 100% 100%;
+      height: 400px;
+    }
+    .navbar-default .navbar-nav>.active>a{
+    background: #ff6600;
+    color: #fff;
+    }
+    .navbar-default .navbar-nav >li>a{
+      color: #003366;
+    }
+    .navbar-default .navbar-nav>.active>a:focus, .navbar-default .navbar-nav>.active>a:hover{
+      color:#fff;
+    }
+    .navbar-default .navbar-nav>li>a:focus, .navbar-default .navbar-nav>li>a:hover{
+      color:#fff;
+    }
+    .dropdown-menu>li>a:focus, .dropdown-menu>li>a:hover{
+      color:#fff;
+    }
+    .slider-content{
+      text-align: center;
+    }
+    .Content-body{
+      width:90%;
+      text-align: center;
+      margin-left: 5%;
+    }
+    .footer{
+      bottom:0;
+      margin-top: 35%;
+      margin-bottom: 0px;
+      background-color: #F2AE7B;
+      height: 100px;
+	  border: 5px solid #F2AE7B;
+	box-shadow: 0px 0px 0px 5px blue;
+    }
+    div.corousel-inner.my-inner {
+        border-top: 10px;
+        border-left: 10px;
+        border-right: 10px;
+        border-bottom: 00px;
+        border-color: #fff;
+        border-style: solid;
+    }
+	.jumbotron{
+	background: #fff;
+	}
+    .item{
+      height: 400px;
+    }
+    .logo{
+      background: url("images/logo1.PNG") no-repeat ;
+      width:850px;
+      background-size: 100%;
+      display: inline-block;
+      height: 82px;
+    }
+    input[type=number]::-webkit-inner-spin-button,
+input[type=number]::-webkit-outer-spin-button {
+  -webkit-appearance: none;
+  margin: 0;
+}
+	.jumbotronEffecct{
+	background: url("images/BGGIF.gif") repeat;
+	}
+	
+	.borders {
+	border: 5px solid orange;
+	box-shadow: 0px 0px 0px 5px blue;
+	}
+    </style>
+  </head>
+  <body>
+
+    <div id="wrapper" class="">
+
+        <!-- Sidebar -->
+        <div id="sidebar-wrapper">
+            <ul class="sidebar-nav">
+                <li class="sidebar-brand">
+                    <a href="#">
+                        HR Symphony `15
+                    </a>
+                </li>
+                <li><a href="index.html" class="hvr-rectangle-out">Home</a></li>
+                <li><a href="events.html" class="hvr-rectangle-out">Events</a></li>
+                <!--<li><a href="StuProfile.html" class="hvr-rectangle-out">Student Profile</a></li>-->
+                <li><a href="contact.php" class="hvr-rectangle-out">Contact Us</a></li>
+            </ul>
+        </div>
+        <!-- /#sidebar-wrapper -->
+
+        <!-- Page Content -->
+        <div id="page-content-wrapper">
+
+            <div class="jumbotron jumbotronEffecct borders"  style="opacity:1; border-radius:10px; height:150px;">
+              
+                <div class="col-md-1-12">
+                <div class="col-md-1"><img src="images/logo1.PNG" class="" style="width:100px; margin-top: -18%; position: absolute; clip: rect(0px,110px,200px,0px);"></div>
+                <div class="col-md-8"><h2 class=" wow bounceInRight animated" style="padding-left:20px; color:white; font-family: 'Montserrat', sans-serif; margin-left:10%;" > <b>Welcome</b>, HR Symphony `15</h2></div>
+				<div class="col-md-1"></div>
+                <div class="col-md-1"><a href="#menu-toggle" style ="float:right; width:80px; margin-right:25px; margin-top:15px;" value="Menu" class="col-md-1 btn btn-primary" id="menu-toggle"> Menu</a>
+				</div></div>
+            </div>
+            <div class="Content-body">
+              <div class="container jumbotron">
+                <section>
+                  <div class="fellowDetails">
+                    <div class="member col-md-12" style="border-radius: 25px;  border: 2px solid #73AD21;">
+					<div class="col-md-1">&nbsp;</div>
+                      <div class="col-md-3" style="text-align:left; border:1px solid #000;">
+                        <label>Name : </label> <h4>Amal Abishek</h4>
+                        <label>Contact :</label><h4>+91 - 91765 19926</h4>
+                      </div>
+					  <div class="col-md-3" style="text-align:left; border:1px solid #000;">
+                        <label>Name : </label> <h4>Hemnath </h4>
+                        <label>Contact :</label><h4>+91 - 91506 86920</h4>
+                      </div>
+                      <div class="col-md-3" style="text-align:left; border:1px solid #000;">
+                        <label>Name : </label> <h4>Shradha H Chhabria</h4>
+                        <label>Contact :</label><h4>+91 - 98846 77803</h4>
+                      </div>
+                    </div>
+                  </div>
+                </section>
+              <section>
+                <div class="col-lg-8 col-lg-offset-2">
+                    <div class="page-header">
+                        <h2>Send Message</h2>
+                    </div>
+
+                    <form class="form-horizontal" id="defaultForm" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+                        <div class="form-group">
+                            <label class="col-lg-3 control-label">Name</label>
+                            <div class="col-lg-5">
+                                <input type="text" class="form-control" name="name" placeholder="Name" />
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label class="col-lg-3 control-label">Email address</label>
+                            <div class="col-lg-5">
+                                <input type="text" class="form-control" name="email" />
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="col-lg-3 control-label">Mobile</label>
+                            <div class="col-lg-5">
+                                <input type="number" class="form-control" name="number" minlength="10" maxlength="13"/>
+                            </div>
+                        </div>
+						<div class="form-group">
+                            <label class="col-lg-3 control-label">Message</label>
+                            <div class="col-lg-5">
+                                <input type="text" class="form-control" name="msg" maxlength="200"/>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="col-lg-3 control-label" id="captchaOperation"></label>
+                            <div class="col-lg-2">
+                                <input type="text" class="form-control" name="captcha" />
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="col-lg-9 col-lg-offset-1">
+                                <input id="submit" type="submit" class="btn btn-primary" name="submit" value="submit"></button>
+                        </div>
+                        </form>
+						<?php
+						if (isset($_POST['submit'])) { 
+							  $name = $_POST['name'];
+							  $email = $_POST['email'];
+							  $number = $_POST['number'];
+							  $msg = $_POST['msg'];
+							  $ToEmail = 'cyborgroger@gmail.com';
+							  $EmailSubject = 'HR`Symphony contact form '; 
+							  $mailheader = "From: ".$_POST["email"]."\r\n"; 
+							  $mailheader .= "Reply-To: ".$_POST["email"]."\r\n"; 
+							  $mailheader .= "Content-type: text/html; charset=iso-8859-1\r\n"; 
+							  $MESSAGE_BODY = "Name: ".$_POST["name"]."<br>"; 
+							  $MESSAGE_BODY .= "Email: ".$_POST["email"]."<br>"; 
+							  $MESSAGE_BODY .= "Mobile:".$_POST['number']."<br />";  
+							  $MESSAGE_BODY .= "Message: ".nl2br($_POST["msg"])."<br>"; 
+							  if(mail($ToEmail, $EmailSubject, $MESSAGE_BODY, $mailheader))
+							  {
+							  echo "<script>alert('Mail was sent !');</script>";
+							  echo "<script>document.location.href='contact.php'</script>";
+							  }
+							  else
+							  {
+							  echo "<script>alert('Mail was not sent. Please try again later');</script>";
+							  }
+							 }
+							 ?>
+                      </div>
+          </section>
+        </div>
+          <!-- :form -->
+            </div>
+      <br/>
+      
+    </div>
+        <!-- /#page-content-wrapper -->
+    </div>
+	<div class="footer"  style="text-align:center;">
+        <br />
+        <!--facebook &nbsp; Twitter &nbsp; Gmail &nbsp;-->
+        <br />
+        <h4>Designed By : <a href="https://www.webkb.in" Target='_blank' style="text-decoration:none;">WebKB</a></h4>
+
+      </div>
+    <!-- /#wrapper -->
+
+        <script src="https://cdn.jsdelivr.net/jquery/2.1.3/jquery.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <script type="text/javascript" src="dist/js/bootstrapValidator.js"></script>
+    <!-- Menu Toggle Script -->
+    <script>
+    $("#menu-toggle").click(function(e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled");
+    });
+    $("#menu-toggle").click();
+    </script>
+	<script type="text/javascript">
+	$("#submit").on('click',function(event){
+      if ($("#defaultForm").valid(this,'Check Fields')==false){
+        return false;
+      }
+  $(function(){
+    $("#defaultForm").validation({icon:true});
+    $("#submit").on('click',function(event){
+      if ($("#defaultForm").valid(this,'Check Fields')==false){
+        return false;
+      }
+	  else{
+		  return true;
+	  }
+    })
+	</script>
+    
+    <script type="text/javascript">
+    $(document).ready(function() {
+        // Generate a simple captcha
+        function randomNumber(min, max) {
+            return Math.floor(Math.random() * (max - min + 1) + min);
+        };
+        $('#captchaOperation').html([randomNumber(1, 100), '+', randomNumber(1, 200), '='].join(' '));
+
+        $('#defaultForm').bootstrapValidator({
+    //        live: 'disabled',
+            message: 'This value is not valid',
+            feedbackIcons: {
+                valid: 'glyphicon glyphicon-ok',
+                invalid: 'glyphicon glyphicon-remove',
+                validating: 'glyphicon glyphicon-refresh'
+            },
+            fields: {
+                Name: {
+                    group: '.col-lg-5',
+                    validators: {
+                        notEmpty: {
+                            message: 'The Name is required and cannot be empty'
+                        }
+                    }
+                },
+                number: {
+                    validators: {
+                        notEmpty: {
+                            message: 'The number is required and cannot be empty'
+                        }
+                    }
+                },
+                email: {
+                    validators: {
+                      notEmpty: {
+                          message: 'The Email Address is required and cannot be empty'
+                      },
+                        emailAddress: {
+                            message: 'The input is not a valid email address'
+                        }
+                    }
+                },
+
+                captcha: {
+                    validators: {
+                        callback: {
+                            message: 'Wrong answer',
+                            callback: function(value, validator) {
+                                var items = $('#captchaOperation').html().split(' '), sum = parseInt(items[0]) + parseInt(items[2]);
+                                return value == sum;
+                            }
+                        }
+                    }
+                }
+            }
+        });
+    });
+    </script>
+  </body>
+</html>
---
+++ index.php
@@ -0,0 +1,314 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>HR`Symphony</title>
+    <link href="menu/component.css" rel="stylesheet" type="text/css" />
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="roll/script.js"></script>
+    <link rel="stylesheet" href="roll/animate.css" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/octicons/3.1.0/octicons.min.css">
+    <link rel="stylesheet" href="css/simple-sidebar.css">
+    <link href="menu/component.css" rel="stylesheet" type="text/css" />
+    <link href='https://fonts.googleapis.com/css?family=Montserrat:700' rel='stylesheet' type='text/css'>
+    <!--[if lt IE 9]>
+      <script src="https://cdn.jsdelivr.net/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://cdn.jsdelivr.net/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <link rel="stylesheet" href="clock/flipclock.css">
+    <style>
+    body ,#page-content-wrapper{
+      background: transparent url("images/themee.jpg") no-repeat;
+      background-size: cover;
+    }
+
+    header.smaller{
+      background-color: #ccc;
+    }
+    .navbar-default {
+      background: url("http://mssw.in/img/logo_header_bg.png") repeat;
+    }
+    .carousel-inner > .item > img,
+    .carousel-inner > .item > a > img {
+        width: 100%;
+        height:400px;
+        margin: auto;
+    }
+    .carousel {
+      top: 65;
+    }
+    .item1{
+      background:transparent  url("http://www.w3schools.com/bootstrap/img_flower.jpg") no-repeat;
+      background-size: 100% 100%;
+      height: 400px;
+    }
+    .navbar-default .navbar-nav>.active>a{
+    background: #ff6600;
+    color: #fff;
+    }
+    .navbar-default .navbar-nav >li>a{
+      color: #003366;
+    }
+    .navbar-default .navbar-nav>.active>a:focus, .navbar-default .navbar-nav>.active>a:hover{
+      color:#fff;
+    }
+    .navbar-default .navbar-nav>li>a:focus, .navbar-default .navbar-nav>li>a:hover{
+      color:#fff;
+    }
+    .dropdown-menu>li>a:focus, .dropdown-menu>li>a:hover{
+      color:#fff;
+    }
+    .slider-content{
+      text-align: center;
+    }
+    .Content-body{
+      width:90%;
+      text-align: center;
+      margin-left: 5%;
+	  background-color: #fff;
+    }
+	.container{
+	border: 5px solid blue;
+	background-color:#F2AE7B;
+	color: blue;
+	width:100%;
+	}
+   .footer{
+      bottom:0;
+      margin-top: 8%;
+      margin-bottom: 0px;
+      background-color: #F2AE7B;
+      height: 100px;
+	  border: 5px solid #F2AE7B;
+	box-shadow: 0px 0px 0px 5px blue;
+    }
+    div.corousel-inner.my-inner {
+        border-top: 10px;
+        border-left: 10px;
+        border-right: 10px;
+        border-bottom: 00px;
+        border-color: #fff;
+        border-style: solid;
+    }
+    .item{
+      height: 400px;
+    }
+    .logo{
+      background: url("images/logo1.PNG") no-repeat ;
+      width:850px;
+      background-size: 100%;
+      display: inline-block;
+      height: 82px;
+    }
+	.jumbotronEffecct{
+	background: url("images/BGGIF.gif") repeat;
+	}
+	
+	.borders {
+	border: 5px solid orange;
+	box-shadow: 0px 0px 0px 5px blue;
+	}
+	p{
+	color:#fff;
+    text-align: justify;
+    text-justify: inter-word;
+	}
+	@media (min-width: 1200px)
+	.container {
+		width: 100%;
+	}
+	@media screen and (min-width: 768px)
+	.container {
+		width: 100%;
+	}
+    </style>
+  </head>
+  <body>
+
+    <div id="wrapper" class="">
+
+        <!-- Sidebar -->
+        <div id="sidebar-wrapper">
+            <ul class="sidebar-nav">
+                <li class="sidebar-brand">
+                    <a href="#">
+                        HR Symphony `15
+                    </a>
+                </li>
+                <li><a href="#" class="hvr-rectangle-out">Home</a></li>
+                <li><a href="events.html" class="hvr-rectangle-out">Events</a></li>
+                <!--<li><a href="StuProfile.html" class="hvr-rectangle-out">Student Profile</a></li>-->
+                <li><a href="contact.php" class="hvr-rectangle-out">Contact Us</a></li>
+            </ul>
+        </div>
+        <!-- /#sidebar-wrapper -->
+
+        <!-- Page Content -->
+        <div id="page-content-wrapper">
+
+            <div class="jumbotron jumbotronEffecct borders"  style="opacity:1; border-radius:10px; height:150px;">
+              
+                <div class="col-md-1-12">
+                <div class="col-md-1"><img src="images/logo1.PNG" class="" style="width:100px; margin-top: -18%; position: absolute; clip: rect(0px,110px,200px,0px);"></div>
+                <div class="col-md-8"><h2 class=" wow bounceInRight animated" style="padding-left:20px; color:white; font-family: 'Montserrat', sans-serif; margin-left:10%;" > <b>Welcome</b>, HR Symphony `15</h2></div>
+				<div class="col-md-1"></div>
+                <div class="col-md-1"><a href="#menu-toggle" style ="float:right; width:80px; margin-right:25px; margin-top:15px;" value="Menu" class="col-md-1 btn btn-primary" id="menu-toggle"> Menu</a>
+				</div></div>
+            </div>
+            <div class="Content-body">
+            <div id="myCarousel" class="carousel slide" data-ride="carousel">
+            <!-- Indicators -->
+            <ol class="carousel-indicators">
+              <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+              <li data-target="#myCarousel" data-slide-to="1"></li>
+              <li data-target="#myCarousel" data-slide-to="2"></li>
+              <li data-target="#myCarousel" data-slide-to="3"></li>
+			  <li data-target="#myCarousel" data-slide-to="4"></li>
+            </ol>
+
+            <!-- Wrapper for slides -->
+            <div class="carousel-inner item wow slideInUp animated" role="listbox">
+              <div class="item active" style="background: url('images/slider/3.jpg'); background-size: 100% 100%;">
+
+              </div>
+
+              <div class="item wow zoomIn animated" style="background: url('images/slider/5.jpg');filter: brightness(150%); -webkit-filter: brightness(150%); background-size: 100% 100%;">
+                <!--<div class="carousel-caption">
+                   <h1>Changes to the Grid</h1>
+                   <p>Bootstrap 3 still features a 12-column grid, but many of the CSS class names have completely changed.</p>
+                   <p><a class="btn btn-large btn-warning" href="#">Learn more</a></p>
+                </div>-->
+              </div>
+
+              <div class="item wow flipInX animated" style="background: url('images/slider/4.jpg');filter: brightness(150%); -webkit-filter: brightness(150%); background-size: 100% 100%;">>
+               <!-- <div class="carousel-caption">
+                   <h1>Changes to the Grid</h1>
+                   <p>Bootstrap 3 still features a 12-column grid, but many of the CSS class names have completely changed.</p>
+                   <p><a class="btn btn-large btn-warning" href="#">Learn more</a></p>
+                </div>-->
+              </div>
+
+              <div class="item wow flipInY animated" style="background: url('images/slider/1.jpg');filter: brightness(120%); -webkit-filter: brightness(120%); background-size: 100% 100%;">>
+                <div class="carousel-caption">
+                  <!-- <h1>Changes to the Grid</h1>
+                   <p>Bootstrap 3 still features a 12-column grid, but many of the CSS class names have completely changed.</p>
+                   <p><a class="btn btn-large btn-warning" href="#">Learn more</a></p>-->
+                </div>
+              </div>
+			  <div class="item wow rotateIn animated" style="background: url('images/slider/6.jpg'); background-size: 100% 100%;">>
+                <div class="carousel-caption">
+                  <!-- <h1>Changes to the Grid</h1>
+                   <p>Bootstrap 3 still features a 12-column grid, but many of the CSS class names have completely changed.</p>
+                   <p><a class="btn btn-large btn-warning" href="#">Learn more</a></p>-->
+                </div>
+              </div>
+            </div>
+
+            <!-- Left and right controls -->
+            <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+              <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+              <span class="sr-only">Previous</span>
+            </a>
+            <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+              <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+              <span class="sr-only">Next</span>
+            </a>
+          </div>
+
+          <div class="col-md-12" >
+            <div class="col-md-2"></div>
+            <div class="col-md-10"><div class="clock" style="margin:2em;"> </div>  </div>
+          </div>
+
+          <div class="container-content">
+            <div class="left col-md-3 wow bouncein animated" >
+            </div>
+            <div class="right col-md-9 wow fadein animated">
+            </div>
+          </div>
+        </div>
+		
+		<div class="Content-body">
+              <div class="container jumbotron">
+               <section class="CAboutUS">
+					<h3><u>About Our College</u>:</h3>
+					<p> &nbsp; &nbsp; MADRAS SCHOOL OF SOCIAL WORK, established in 1952, located in Chennai, South India, is an Autonomous Institution, NAAC accredited and affiliated to University of Madras. Madras School of Social Work was founded by Mary Clubwala Jadhav under the auspices of Madras State Branch of the Indian Conference of Social Work (renamed the Indian Council of Social Welfare) and the Guild of Service (central). The school is run under the aegis of the Society for Social Education and Research (SSER). The School is rated 3rd Best Social Work College in India and First in South India.</p>
+			   </section>
+			   <section class="DAboutUS">
+					<h3><u>About Our Department</u>:</h3>
+					<p> &nbsp; &nbsp; M. A. HUMAN RESOURCE MANAGEMENT is an intensive two year program aimed at making future HR leaders by preparing a global talent engine, capable of meeting the challenges of the competitive business environment. Students are imbibed with holistic personality skills that enhance their Employability.</p>
+			   </section>
+			   </div>
+                
+          <!-- :form -->
+            </div>
+		
+      </div>
+      <br/>
+      <div class="footer"  style="text-align:center;">
+        <br />
+        <!--facebook &nbsp; Twitter &nbsp; Gmail &nbsp;-->
+        <br />
+        <h4>Designed By : <a href="https://www.webkb.in" Target='_blank' style="text-decoration:none;">WebKB</a></h4>
+
+      </div>
+        <!-- /#page-content-wrapper -->
+    </div>
+    <!-- /#wrapper -->
+
+        <script src="https://cdn.jsdelivr.net/jquery/2.1.3/jquery.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+    <!-- Menu Toggle Script -->
+    <script>
+    $("#menu-toggle").click(function(e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled");
+    });
+    $("#menu-toggle").click();
+    </script>
+ <!--   <script type="text/javascript">
+
+        function init() {
+            window.addEventListener('scroll', function(e){
+                var distanceY = window.pageYOffset || document.documentElement.scrollTop,
+                    shrinkOn = 300,
+                    header = document.querySelector("header");
+                if (distanceY > shrinkOn) {
+                    classie.add(header,"smaller");
+                } else {
+                    if (classie.has(header,"smaller")) {
+                        classie.remove(header,"smaller");
+                    }
+                }
+            });
+        }
+        window.onload = init();
+    </script>-->
+    <script src="clock/flipclock.js"></script>
+    <script type="text/javascript">
+      var clock;
+
+      $(document).ready(function() {
+        var clock;
+
+        clock = $('.clock').FlipClock({
+              clockFace: 'DailyCounter',
+              autoStart: false,
+              callbacks: {
+                stop: function() {
+                  $('.message').html('Launched!')
+                }
+              }
+          });
+
+          clock.setTime(220880);
+          clock.setCountdown(true);
+          clock.start();
+
+      });
+    </script>
+  </body>
+</html>
